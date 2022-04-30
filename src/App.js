48ac0f776d0130bcde58391abf23374f782e6c13
Sergei Anikeev <anikeevsu@gmail.com>
2022-04-30 15:30:23 +1200

    units change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component{
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     if (city) {
       this.setState({
